156e18db023aaeb0a9c302b0ff099478c452fccc
Oka Bayu Pratama <okabayu1026@gmail.com>
2025-08-31 04:05:56 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDWxO8Wq6I6dVig1cocVwj8eSO_Mqvl

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDWxO8Wq6I6dVig1cocVwj8eSO_MqvlvFQ",
   authDomain: "garda-lestari-5gz3p.firebaseapp.com",
   projectId: "garda-lestari-5gz3p",
-  storageBucket: "garda-lestari-5gz3p.appspot.com",
+  storageBucket: "garda-lestari-5gz3p.firebasestorage.app",
   messagingSenderId: "120282807854",
   appId: "1:120282807854:web:71c1d51656afb8af2929bf"
 };
