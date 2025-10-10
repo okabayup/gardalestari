23f7fcf6011a1a7516234e469d4e565de24614ab
Oka Bayu Pratama <okabayu1026@gmail.com>
2025-10-10 13:57:49 +0000

    Sepertinya kamu membuat folder workspace yang tidak berhubungan sama sek

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  fallbacks: {
+    document: '/offline.html',
+  },
 });
 
 
