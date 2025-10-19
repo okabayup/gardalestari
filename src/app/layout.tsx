3b580c3981b601266b9670107e03a57c004df4ac
Oka Bayu Pratama <okabayu1026@gmail.com>
2025-10-19 03:54:40 +0000

    Make your app faster and more reliable by adding a service worker.
    Add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
