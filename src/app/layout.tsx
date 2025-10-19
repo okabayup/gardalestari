da20616d0030a0b87a80a8cd5dcf80d7eae99f60
Oka Bayu Pratama <okabayu1026@gmail.com>
2025-10-19 04:07:21 +0000

    Saya juga telah menambahkan icon-bg-putih.png barangkali digunakan untuk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,11 @@ export async function generateMetadata(): Promise<Metadata> {
     },
     description: 'Garda Lestari adalah organisasi kepemudaan yang berfokus pada inovasi di sektor agro-maritim dan kehutanan untuk pembangunan berkelanjutan di Indonesia.',
     manifest: '/manifest.json',
+    themeColor: '#347C45',
     icons: {
-      icon: '/logo.png',
-      shortcut: '/favicon.ico',
-      apple: '/icons/icon-192x192.png',
+      icon: '/icon-bg-putih.png',
+      shortcut: '/icon-bg-putih.png',
+      apple: '/logo-bg-hijau.png',
     },
     openGraph: {
       title: 'Garda Lestari: Aksi Nyata Anda untuk Bumi',
@@ -64,14 +65,12 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
-        <meta name="theme-color" content="#347C45" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
