9bfb83e772a56fc7296b3ccbf70683b14c8ea994
Oka Bayu Pratama <okabayu1026@gmail.com>
2026-02-25 15:13:39 +0000

    🚨 *Garda Lestari App Alert* 🚨
    🚨 *Garda App Error* 🚨
    
    *Context:* glo

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
+
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -19,14 +26,13 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
     ],
   },
 };
 
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(nextConfig);
+export default withPWA(nextConfig);
